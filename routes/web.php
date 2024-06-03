5676aa82bdfdb851d3c0ef2969d5fa8a9b4d50f4
Yoga Yudha Tama <yogayudhatama48@gmail.com>
2024-06-03 15:57:39 +0700

    weather-app

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http; 
 
 Route::get('/', function () {
+    $locations = ['Jakarta', 'Toronto', 'Canada', 'Tokyo']; 
+    $apiKey = 'a1db5e0d1559fbde16dbe4224cc4f7a5'; 
+
+    $weatherData = []; 
+
+    foreach ($locations as $location) {
+        $response = Http::get("https://api.openweathermap.org/data/2.5/weather?q={$location}&appid={$apiKey}");
+        
+        $weatherData[$location] = $response->json();
+    }
+
+    dump($weatherData); 
+
     return view('welcome');
 });
